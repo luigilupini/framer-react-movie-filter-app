60317b64b7461772ab5c55d13a3ec76d7bc6febe
Luigi Lupini <luigi.lupini@gmail.com>
2022-10-18 14:56:35 +0200

    #1 - Basic App structure and fetching all content

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,30 @@ import { useState, useEffect } from "react";
 import Movie from "./components/Movie";
 
 import "./App.css";
-import Filter from "./components/Filter";
-
-const api = "d0f6a962c312488745b97f69b178e4b5";
 
 function App() {
-  const [popular, setPopular] = useState([]);
+  const [movies, setMovies] = useState([]); // original (we don't mutate it)
+
   const fetchPopular = async () => {
+    const url = "https://api.themoviedb.org/3/movie";
+    const api = "d0f6a962c312488745b97f69b178e4b5";
+    const sortBy = "popular";
     const data = await fetch(
-      `https://api.themoviedb.org/3/movie/popular?api_key=${api}&language=en-US&page=1`
+      `${url}/${sortBy}?api_key=${api}&language=en-US&page=1`
     );
     const movies = await data.json();
-    setPopular(movies.results);
+    setMovies(movies.results);
   };
   useEffect(() => {
     fetchPopular();
   }, []);
-  console.log(popular.results);
+  console.log(movies.results);
   return (
     <div className="App">
-      <Filter />
       <div className="popular-movies">
-        {popular.map((movie) => {
-          return <Movie key={movie.id} movie={movie} />;
-        })}
+        {movies.map((movie) => (
+          <Movie key={movie.id} movie={movie} />
+        ))}
       </div>
     </div>
   );
